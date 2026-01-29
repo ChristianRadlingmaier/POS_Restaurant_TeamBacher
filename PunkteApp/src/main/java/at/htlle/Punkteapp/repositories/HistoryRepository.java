cfbba524e7b8ba3e3ae9f733aa308ecf287a6d0c
Nevio Peissl <211witb19@o365.htl-leoben.at>
2026-01-29 09:03:05 +0100

    Added explanation for repositorys

--- PunkteApp/src/main/java/at/htlle/Punkteapp/repositories/HistoryRepository.java
+++ PunkteApp/src/main/java/at/htlle/Punkteapp/repositories/HistoryRepository.java
@@ -8,3 +8,15 @@ import java.util.List;
 public interface HistoryRepository extends JpaRepository<History,Long> {
     List<History> findByUser_UserIdOrderByDateDesc(Long userId);
 }
+
+
+/**
+ * 
+ * Spring Data JPA Namenskonvention
+ * 
+ * 
+ *   SELECT * FROM History h
+ *   JOIN Users u ON h.user_id = u.user_id
+ *   WHERE u.user_id = ?
+ *   ORDER BY h.date DESC
+ */
